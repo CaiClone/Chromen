4db8162285ba0647589f623bf0e931d648c40041
CaiSkove <caiskove@gmail.com>
2020-10-03 20:08:30 +0200

    Juice Story

--- Assets/Scripts/Gameplay/Story/StoryManager.cs
+++ Assets/Scripts/Gameplay/Story/StoryManager.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Yarn.Unity;
 
 public class StoryManager : MonoBehaviour
 {
     public bool MusicOn = true;
+    public TextMeshProUGUI renderText;
+    public GameObject image;
     private void Start()
     {
         if (MusicOn)
@@ -15,5 +19,35 @@ public class StoryManager : MonoBehaviour
     public void PlayClick()
     {
         AudioController.Instance.Play("click");
+    }
+
+    [YarnCommand("setcolorDad")]
+    public void setcolorDad()
+    {
+        renderText.color = Color.red;
+    }
+
+    [YarnCommand("setcolorMe")]
+    public void setcolorMe()
+    {
+        renderText.color = Color.white;
+    }
+
+    [YarnCommand("ShowImage")]
+    public void showImage()
+    {
+        image.SetActive(true);
+        StartCoroutine(fadeInImage());
+    }
+
+    IEnumerator fadeInImage(float speed = 0.3f)
+    {
+        var img = image.GetComponent<SpriteRenderer>();
+        for (float i = 0; i <= 1; i += Time.deltaTime * speed)
+        {
+            // set color with i as alpha
+            img.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
     }
 }
