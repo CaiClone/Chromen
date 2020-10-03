297918c4e4db34f3530c4256eb0d3ae554eb3a65
CaiSkove <caiskove@gmail.com>
2020-10-03 19:18:37 +0200

    Add Story Section with dialogues and clickity clickity sound

---
+++ Assets/Scripts/Gameplay/Story/StoryManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour
+{
+    public void PlayClick()
+    {
+        AudioController.Instance.Play("click");
+    }
+}
