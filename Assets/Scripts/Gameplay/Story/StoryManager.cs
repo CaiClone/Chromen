b56656b5d71f7812693e58dbadbcfa1d2de7c7b1
CaiSkove <caiskove@gmail.com>
2020-10-03 19:39:05 +0200

    Music

--- Assets/Scripts/Gameplay/Story/StoryManager.cs
+++ Assets/Scripts/Gameplay/Story/StoryManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class StoryManager : MonoBehaviour
 {
+    public bool MusicOn = true;
+    private void Start()
+    {
+        if (MusicOn)
+        {
+            Utils.WaitAndRun(1, () => AudioController.Instance.Play("Song1", true));
+        }
+    }
     public void PlayClick()
     {
         AudioController.Instance.Play("click");
