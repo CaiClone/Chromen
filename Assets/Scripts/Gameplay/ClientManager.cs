4984930e8cafbfd75bcc64106449f96dbe3a4e80
CaiSkove <caiskove@gmail.com>
2020-10-03 04:09:21 +0200

    Add clients at runtime

---
+++ Assets/Scripts/Gameplay/ClientManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ClientManager : MonoBehaviour
+{
+    List<Transform> clientPositions = new List<Transform>();
+    GameObject baseClient;
+    void Start()
+    {
+        baseClient = Resources.Load<GameObject>("Prefabs/Client");
+        foreach(Transform pos in transform)
+        {
+            clientPositions.Add(pos);
+        }
+
+        foreach(var pos in clientPositions)
+        {
+            Instantiate(baseClient, pos);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
