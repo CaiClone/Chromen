b8a8ebe6b8f7d3b17b6ab073fddbe458d6500a0b
CaiSkove <caiskove@gmail.com>
2020-10-04 21:26:59 +0200

    Start Menu

---
+++ Assets/Scripts/Core/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public SpriteRenderer logo;
+    public Sprite newlogo;
+    public void Start()
+    {
+        if (GameState.Instance.loopcount >= 1)
+        {
+            logo.sprite = newlogo;
+        }
+    }
+    public void LoadScene()
+    {
+        AudioController.Instance.Play("beep");
+        Utils.WaitAndRun(0.5f, () => SceneManager.LoadScene("Story"));
+    }
+}
